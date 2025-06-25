cb7bfbd88e53c3108bcf45d4e880aca4b0659de6
Soumi-Code <soumiray.11sc.2020@gmail.com>
2025-06-25 20:25:49 +0530

    Cart Drawer Creation

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from 'react-icons/hi2'
 import SearchBar from './SearchBar'
+import CartDrawer from '../Layout/CartDrawer'
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(true);
+
+    const toggleCartDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    }
+
+
   return (
     <div className='container mx-auto flex items-center justify-between py-4 px-6'>
       {/* Left - Logo */}
@@ -30,7 +38,7 @@ const Navbar = () => {
         <Link to='/profile' className='hover:text-black'>
         <HiOutlineUser className='h-6 w-6 text-gray-700'/>
         </Link>
-        <button className='relative hover:text-black'>
+        <button onClick={toggleCartDrawer}className='relative hover:text-black'>
           <HiOutlineShoppingBag className='h-6 w-6 text-gray-700' />
           <span className='absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5'>
             2
@@ -43,7 +51,9 @@ const Navbar = () => {
           <HiBars3BottomRight className='h-6 w-6 text-gray-700' />
         </button>
       </div>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
     </div>
+  
   )
 }
 
