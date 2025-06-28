20786729f3cf5ea508ad4e306c480c6b7ea9be8d
Soumi-Code <soumiray.11sc.2020@gmail.com>
2025-06-28 19:35:20 +0530

    Adding banner for gender collection

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -9,20 +9,20 @@ const Footer = () => {
   return (
     <footer className='border-t py-12'>
         <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0'>
-            <div>
-                <h3 className='text-lg text-gray-800 mb-4'>Newsletter</h3>
-                <p className='text-gray-500 mb-4'>Be the first to hear about new products, exclusive offers, and more.</p>
-                <p className='font-medium text-sm text-gray-600 mb-6'>Sign up and get 10% off your first order</p>
+            <div className='px-6'>
+                <h3 className='text-lg text-gray-800 mb-4 px-6'>Newsletter</h3>
+                <p className='text-gray-500 mb-4 px-6'>Be the first to hear about new products, exclusive offers, and more.</p>
+                <p className='font-medium text-sm text-gray-600 mb-6 px-6'>Sign up and get 10% off your first order</p>
                 <form className='flex'>
                     <input type='email'
                     placeholder='Enter your email'
-                    className='p-3 w-full text-sm border-t border-l border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all' required />
+                    className='p-3 w-full text-sm border-t border-l border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all px-6' required />
                     <button type='submit' className='bg-black text-white px-6 py-3 text-sm rounded-r-md hover:bg-gray-800 transition-all'>
                         Subscribe
                     </button>
                 </form>
             </div>
-            <div>
+            <div className='px-8'>
                 <h3 className='text-lg text-gray-800 mb-4'>Shop</h3>
                 <ul className='space-y-2 text-gray-600'>
                     <li>
@@ -40,7 +40,7 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h3 className='text-lg text-gray-800 mb-4'>Shop</h3>
+                <h3 className='text-lg text-gray-800 mb-4'>Support</h3>
                 <ul className='space-y-2 text-gray-600'>
                     <li>
                         <Link to='#' className='hover:text-gray-5000 transition-colors'>Contact Us</Link>
