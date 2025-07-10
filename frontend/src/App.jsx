86fae5c7b0c7e9c834cbb6cda8b3747257b89be5
Soumi-Code <soumiray.11sc.2020@gmail.com>
2025-07-10 09:09:19 +0530

    Order Management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,9 @@ import AdminHomePage from './pages/AdminHomePage';
 import UserManagement from './components/Admin/UserManagement';
 import ProductManagement from './components/Admin/ProductManagement';
 import EditProductPage from './components/Admin/EditProductPage';
+import OrderManagement from './components/Admin/orderManagement';
+
+
 
 const App = () => {
   return (
@@ -41,6 +44,7 @@ const App = () => {
       <Route path="users" element={<UserManagement/>}/>
       <Route path="products"element={<ProductManagement/>}/>
       <Route path="products/:id/edit"element={<EditProductPage/>}/>
+      <Route path="orders"element={<OrderManagement />}/>
       </Route>
     </Routes>
     </BrowserRouter>  )
