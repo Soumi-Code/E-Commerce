45be5a65ead804ed52a95f35b752953ed674b267
Soumi-Code <soumiray.11sc.2020@gmail.com>
2025-07-13 09:45:28 +0530

    Data Seeding

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const Product = require('../models/Product');
-const { protect } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 //@route POST/api/products
 //@desc Create a new product
 //@access Private/Admin
-router.post('/', protect, async (req, res) => {
+router.post('/', protect, admin, async (req, res) => {
     try {
         const {
             name,
@@ -61,5 +61,85 @@ router.post('/', protect, async (req, res) => {
         res.status(500).send('Server Error');
     }
 })
+
+//@route PUT/api/products/:id
+//@desc Update a product
+//@access Private/Admin
+router.put('/:id', protect, admin, async (req, res) => {
+    try {
+        const {
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            sizes,
+            colors,
+            collections,
+            material,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            tags,
+            dimensions,
+            weight,
+            sku,
+        } = req.body;
+
+        const product = await Product.findById(req.params.id);
+        if (product) {
+        // Update product fields
+        product.name = name || product.name;
+        product.description = description || product.description;
+        product.price = price || product.price;
+        product.discountPrice = discountPrice || product.discountPrice;
+        product.countInStock = countInStock || product.countInStock;
+        product.category = category || product.category;
+        product.brand = brand || product.brand;
+        product.sizes = sizes || product.sizes;
+        product.colors = colors || product.colors;
+        product.collections = collections || product.collections;
+        product.material = material || product.material;
+        product.gender = gender || product.gender;
+        product.images = images || product.images;
+        product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured;
+        product.isPublished = isPublished !== undefined ? isPublished : product.isPublished;
+        product.tags = tags || product.tags;
+        product.dimensions = dimensions || product.dimensions;
+        product.weight = weight || product.weight;
+        product.sku = sku || product.sku;
+        
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else{
+        res.status(404).json({ message: 'Product not found' });
+    }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route DELETE/api/products/:id
+//@desc Delete a product by ID
+//@access Private/Admin
+router.delete('/:id', protect, admin, async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            await product.deleteOne();
+            res.json({ message: 'Product removed' });
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
---
+++ backend/seeder.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Product = require('./models/Product');
+const User = require('./models/User');
+const products = require('./data/products');
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+
+//Function to seed data
+
+const seedData = async () => {
+    try {
+        // Clear existing data
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        // Create admin user
+        const createdUser = await User.create({
+            name: 'Admin User',
+            email: 'admin@example.com',
+            password: '123456',
+            role: 'admin',
+        });
+
+        const userId = createdUser._id;
+
+        // Create sample products
+        const sampleProducts = products.map((product) =>{ return {...product, user: userId }
+        });
+
+        await Product.insertMany(sampleProducts);
+
+        console.log('Data seeded successfully');
+        process.exit();
+    } catch (error) {
+        console.error('Error seeding data:', error);
+        process.exit(1);
+    }
+};
+
+// Run the seed function
+seedData();
