0488d4ade4cb2f8bc5a0d24bcdc5e4d3c258dcc5
Soumi-Code <soumiray.11sc.2020@gmail.com>
2025-07-19 21:31:37 +0530

    Checkout and order Slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productsSlice';
 import cartReducer from './slices/cartSlice';
+import checkoutReducer from './slices/checkoutSlice';
+import orderReducer from './slices/orderSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
     cart: cartReducer,
+    orders: orderReducer,
+    checkout: checkoutReducer,
   },
 });
 
